7e4ffb485ba75069f3ff6abab40b487895df3c1f
Sam D <dsaeresearch@gmail.com>
2021-11-28 13:52:35 -0500

    0.1.1 - Added empty web app for initial commit.

---
+++ AlgoraArt.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlgoraArt.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
